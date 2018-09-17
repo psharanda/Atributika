d7ec6c0c036386c1dcca59294040c3e1830f8854
Pavel Sharanda <edvaef@gmail.com>
2018-09-17 23:58:08 +0300

    fix osx tests

--- Tests/AtributikaTests/AtributikaTests.swift
+++ Tests/AtributikaTests/AtributikaTests.swift
@@ -497,7 +497,7 @@ class AtributikaTests: XCTestCase {
     
     func testTuner() {
         
-        func hexStringToUIColor (hex:String) -> UIColor {
+        func hexStringToUIColor (hex:String) -> Color {
             var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
             
             if (cString.hasPrefix("#")) {
@@ -505,13 +505,13 @@ class AtributikaTests: XCTestCase {
             }
             
             if ((cString.count) != 6) {
-                return UIColor.gray
+                return Color.gray
             }
             
             var rgbValue:UInt32 = 0
             Scanner(string: cString).scanHexInt32(&rgbValue)
             
-            return UIColor(
+            return Color(
                 red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
                 green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
                 blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
